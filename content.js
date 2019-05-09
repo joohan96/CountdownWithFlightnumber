53ed3ccb5f2ef47d98bc86117d5b25ced9dbc4d0
Andrew Oh <joohan0311@hotmail.com>
2019-05-08 23:49:59 -0700

    Fixed google chrome problem. Still need to do extensive form error checking. Still need to use the time given by the API if user inputs expected return date within 24hours

--- content.js
+++ content.js
@@ -1,75 +1,51 @@
-function getMessage(returnDate) {
-    return new Promise(function (resolve, reject) {
-        //1bd984-705fbc;; 
-        //http://aviation-edge.com/api/public/routes?key=1bd984-705fbc&flightNumber=AC30
+function getDistance(returnDate) {
+    var countDownDate = new Date(returnDate);
+    var now = new Date().getTime();
 
-        var countDownDate = new Date(returnDate);
-        var now = new Date().getTime();
+    var distance = countDownDate - now;
 
-        var distance = countDownDate - now;
-
-        // Time calculations for days, hours, minutes and seconds
-        var days = Math.floor(distance / (1000 * 60 * 60 * 24));
-        var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
-        var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
-        var seconds = Math.floor((distance % (1000 * 60)) / 1000);
-
-        if (distance < 0) {
-            resolve("Stop playing! She's already back :)");
-        } else {
-            resolve("Your bb comes back in " + days + "days " + hours + "hours " + minutes + "minutes " + seconds + "seconds!!!");
-        }
-    });
-}
-
-function parseFlightData(resp) {
-    return new Promise(function (resolve, reject) {
-        var data = JSON.parse(resp);
-        console.log(data);
-        var msg = "Your bb will be arriving at " +
-        data[0].arrivalTime + " at "
-        + data[0].arrivalIata + " airport terminal "
-        + data[0].arrivalTerminal;
-        console.log(msg);
-        resolve(msg);
-    });
-}
-
-function getRouteDetails(url) {
-    return new Promise(function (resolve, reject) {
-        console.log("im here nig")
-        var xhr = new XMLHttpRequest();
-        xhr.timeout = 10000;
-        xhr.onreadystatechange = function () {
-            if (xhr.readyState === 4) {
-                if (xhr.status === 200) {
-                    console.log(xhr);
-                    console.log(xhr.response);
-                    resolve(xhr.response);
-                } else {
-                    reject(xhr.status);
-                }
-            }
-        }
-        xhr.ontimeout = function () {
-            console.log("The request for " + url + " timed out.");
-        };
-        xhr.open('GET', url, true);
-        xhr.send(null);
-    })
+    return distance;
 }
 
 chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-    // getMessage(request)
-    console.log(request);
-    const arr = request.split(/(\d+)/);
-    const url = 'https://aviation-edge.com/v2/public/routes?key=1bd984-705fbc&airlineIata=' + arr[0] + '&flightNumber=' + arr[1];
-    console.log(url);
-    getRouteDetails(url)
-        .then(function(response) {
-            parseFlightData(response).then(function (msg) {
-                console.log(msg);
-                sendResponse({msg: msg});
+    const countDownDate = request[0];
+    const flightNumber = request[1];
+
+    const distance = getDistance(countDownDate);
+
+    var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+    var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+    var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+    var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+    if (distance < 0) {
+        sendResponse({ msg: "\nStop playing! She's already back :)" });
+    } else if (days > 0) {
+        sendResponse({ msg: "\nUnfortunately, we cannot track your flight at this time, but your bb comes back in " + days + "days " + hours + " hours " + minutes + " minutes " + seconds + " seconds!!! \n" });
+    } else {
+        const arr = flightNumber.split(/(\d+)/);
+        const url = 'https://aviation-edge.com/v2/public/routes?key=1bd984-705fbc&airlineIata=' + arr[0] + '&flightNumber=' + arr[1];
+        console.log(url);
+        fetch(url)
+            .then(function (response) {
+                if (response.status !== 200) {
+                    console.log('Looks like there was a problem. Status Code: ' + response.status);
+                    return;
+                }
+
+                response.json().then(function (data) {
+                    console.log(data);
+                    const message = "\nYour bb will be arriving at " +
+                        data[0].arrivalTime + " at "
+                        + data[0].arrivalIata + " airport terminal "
+                        + data[0].arrivalTerminal + ". That is in " + hours + " hours " + minutes + " minutes " + seconds + " seconds!!! \n";
+                    sendResponse({ msg: message });
+                });
             })
-        })
+            .catch(function (err) {
+                console.log('Fetch Error :-S', err);
+            });
+    };
+
+    return true;
 })
--- popup.js
+++ popup.js
@@ -6,17 +6,30 @@
 
 document.addEventListener('DOMContentLoaded', function () {
   document.querySelector('button').addEventListener('click', onclick, false)
-  
-  function onclick () {
-    chrome.tabs.query({currentWindow: true, active: true}, function (tabs) {
-      //const countDownDate = document.getElementById("returnDate").value;
-      const flightNumber = document.getElementById("flightNumber").value;
-      console.log(flightNumber);
-      chrome.tabs.sendMessage(tabs[0].id, flightNumber, setMessage)
-    }) 
+
+  function validateForm() {
+    var a = document.getElementById("returnDate").value;
+    var b = document.getElementById("flightNumber").value;
+    if (a == null || a == "", b == null || b == "") {
+      alert("All fields must be filled!!!");
+      return false;
+    } else {
+      return true;
+    }
   }
-  
-  function setMessage (res) {
+
+  function onclick() {
+    if (validateForm()) {
+      chrome.tabs.query({ currentWindow: true, active: true }, function (tabs) {
+        const countDownDate = document.getElementById("returnDate").value;
+        const flightNumber = document.getElementById("flightNumber").value;
+        const arr = [countDownDate, flightNumber];
+        chrome.tabs.sendMessage(tabs[0].id, arr, setMessage)
+      })
+    }
+  }
+
+  function setMessage(res) {
     const div = document.createElement('div')
     div.textContent = `${res.msg}`
     document.body.appendChild(div)
